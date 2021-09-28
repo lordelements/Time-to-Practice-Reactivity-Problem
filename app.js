037be47fa0190c15f289250edfb34348db803856
Andrian Elemento <andrianelemento41@gmail.com>
2021-09-28 11:00:27 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+      methods: {
+          add(num) {
+          this.counter = this.counter + num;
+          },
+          add(num) {
+          this.counter = this.counter + num;
+          },
+      },
+});
+app.mount('#assignment');
